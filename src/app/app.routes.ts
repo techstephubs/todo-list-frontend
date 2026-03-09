b021160941b8f6becc040b6f70ae52ee67454e27
techstephubs <tech@stephubs.com>
2026-03-09 11:52:39 -0300

    feat(multirepo-4): integrate MULTIREPO-4 approved scope into parent branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,26 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
+      import('./pages/index/index.component').then((m) => m.IndexComponent),
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
       import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'todos',
+    loadComponent: () =>
+      import('./pages/todos/todos.component').then((m) => m.TodosComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
   },
 ];
